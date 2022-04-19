8128f1d26f4578e49df5547824fee495bb58a84b
TsTsvetkoff <mukser@yahoo.com>
2022-04-19 23:00:44 +0300

    HM3 Task4 is completed

---
+++ HM_3/task_4.py
@@ -0,0 +1,22 @@
+"""
+Write a Python program which concatenates two text files . In the resulting file separate each in input
+file’s content by a destinctive separator line (e.g. “---------------------------------”)
+"""
+
+read_only_file_1 = open("task_4_read_file_1.txt", 'r')
+read_only_file_2 = open("task_4_read_file_2.txt", 'r')
+
+lines = []
+for line in read_only_file_1:
+    lines.append(line)
+
+lines.extend("---------------------------------------------------------------------------------------------------")
+
+for line2 in read_only_file_2:
+    lines.append(line2)
+
+concatenated_file = open("task_4_read_file_output.txt", 'w')
+
+for item in lines:
+    concatenated_file.write(item)
+
