efd1a09aa45544319ff6f39e05011cdd8091bd74
TsTsvetkoff <mukser@yahoo.com>
2022-04-19 23:10:35 +0300

    HM3 Task5 is completed

---
+++ HM_3/task_5.py
@@ -0,0 +1,10 @@
+"""
+Use the requests library to send a get request to some website of your choice. Save the response
+content(text) to a text file.
+"""
+
+import requests
+
+get = requests.get("https://dir.bg/")
+response = open("task_5_output.txt", 'w')
+response.write(str(get))
