428922ecf76d976a0949ffc855a687561ed2e872
tsvetoslav.tsvetkov <tsvetoslav.tsvetkov@acronis.com>
2022-04-22 23:54:10 +0300

    HM4_Task_5 minor update

--- HM_4/1-6_Tasks/task_5.py
+++ HM_4/1-6_Tasks/task_5.py
@@ -7,11 +7,8 @@
 class Bulgarian():
     @staticmethod
     def print_nationality():
-        print("България над всичко")
+        return "България над всичко"
 
 
 new_bulgarian = Bulgarian()
 print(new_bulgarian.print_nationality())
-#print(Bulgarian.print_nationality())
-
-# Question why "None" is printed - is it due to " Static methods are called static because they always return None."
