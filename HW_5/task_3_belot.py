ab6ac282a5b6fc3cf1376cd06e76f877922874ad
tsvetoslav.tsvetkov <tsvetoslav.tsvetkov@acronis.com>
2022-05-01 01:32:51 +0300

    HM5 - task 3 in progress

--- HW_5/task_3_belot.py
+++ HW_5/task_3_belot.py
@@ -1,5 +1,3 @@
-import numpy as np
-from itertools import zip_longest
 card_points =['7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 card_signs =['Пика', 'Купа', 'Каро', 'Спатия']
 
@@ -20,7 +18,7 @@ def create_deck():
     return deck
 
 
-def play():
+def give_cards():
     deck = create_deck()
     for index in range(1, 5):
         for _ in range(8):
@@ -29,16 +27,21 @@ def play():
 
 def players_cards():
     print(all_given_cards)
+    player_4.append(all_given_cards[0:8])
+    player_3.append(all_given_cards[8:16])
+    player_2.append(all_given_cards[16:24])
+    player_1.append(all_given_cards[24:32])
     for index in range(1, 5):
         print(f"\nplayer {index}:")
         for _ in range(8):
-            if index == 1:
-                player_1.append(all_given_cards.pop())
+            print(all_given_cards.pop())
 
-
-
-play()
+give_cards()
 players_cards()
-print(player_1)
+print(f"Player 1 cards are {player_1}")
+print(f"Player 2 cards are {player_2}")
+print(f"Player 3 cards are {player_3}")
+print(f"Player 4 cards are {player_4}")
+
 
 
