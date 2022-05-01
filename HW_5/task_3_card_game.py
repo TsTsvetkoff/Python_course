da845b5ce787b4d09d9c278923b068ece30a597c
tsvetoslav.tsvetkov <tsvetoslav.tsvetkov@acronis.com>
2022-05-01 16:15:56 +0300

    card game in progress

---
+++ HW_5/task_3_card_game.py
@@ -0,0 +1,108 @@
+card_points = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+card_signs = ['Пика', 'Купа', 'Каро', 'Спатия']
+
+
+all_given_cards = []
+given_players_cards = []
+player_1 = []
+player_2 = []
+player_3 = []
+player_4 = []
+current_hand = {"Player1 card": [], "Player2 card": [], "Player3 card": [], "Player4 card": []}
+
+
+def create_deck():
+    deck = set()
+    for suite in card_signs:
+        suite_cards = {f'{rank} {suite}' for rank in card_points}
+        deck.update(suite_cards)
+    return deck
+
+
+def give_cards():
+    deck = create_deck()
+    for index in range(1, 5):
+        for _ in range(8):
+            all_given_cards.append(deck.pop())
+
+
+def players_cards():
+    print(all_given_cards)
+    player_4.extend(all_given_cards[0:8])
+    player_3.extend(all_given_cards[8:16])
+    player_2.extend(all_given_cards[16:24])
+    player_1.extend(all_given_cards[24:32])
+    for index in range(1, 5):
+        print(f"\nplayer {index}:")
+        for _ in range(8):
+            print(all_given_cards.pop())
+    print(f"Player 1 cards are {player_1}")
+    print(f"Player 2 cards are {player_2}")
+    print(f"Player 3 cards are {player_3}")
+    print(f"Player 4 cards are {player_4}")
+
+
+def other_players_play():
+    player_1_first_card = player_1.pop()
+    current_hand["Player1 card"].append(player_1_first_card)
+
+    first_color_player1 = []
+    for x in player_1_first_card:
+        if ord(x[0]) > 128:
+            first_color_player1.append(x)
+    updated_list = ("".join(first_color_player1))
+    print(f"The color that all must match is: {updated_list}")
+
+    str_match = [s for s in player_2 if s.__contains__(updated_list)]
+    if len(str_match) > 0:
+        current_hand["Player2 card"].append(str_match[0])
+        player_2.remove(str_match[0])
+    else:
+        player_2_first_card = player_2.pop()
+        current_hand["Player2 card"].append(player_2_first_card)
+
+    str_match_3 = [s for s in player_3 if s.__contains__(updated_list)]
+    if len(str_match_3)>0:
+        current_hand["Player3 card"].append(str_match_3[0])
+        player_3.remove(str_match_3[0])
+    else:
+        player_3_first_card = player_3.pop()
+        current_hand["Player3 card"].append(player_3_first_card)
+
+    str_match_4 = [s for s in player_4 if s.__contains__(updated_list)]
+
+    if len(str_match_4)> 0:
+        current_hand["Player4 card"].append(str_match_4[0])
+        player_4.remove(str_match_4[0])
+    else:
+        player_4_first_card = player_4.pop()
+        current_hand["Player4 card"].append(player_4_first_card)
+
+    print(current_hand)
+
+
+def winer_is():
+    all_trown_cards = []
+    for v in current_hand.values():
+        all_trown_cards.append(v)
+    print(all_trown_cards)
+
+    str_match_all = [s for s in all_trown_cards if s.__contains__(updated_list)]
+    print(str_match_all)
+
+
+    #
+    # print("------------------------------------------------------------------")
+    # print(f"Player 1 cards now are {player_1}")
+    # print(f"Player 2 cards now are {player_2}")
+    # print(f"Player 3 cards now are {player_3}")
+    # print(f"Player 4 cards now are {player_4}")
+    #
+
+give_cards()
+players_cards()
+other_players_play()
+
+
+
+
