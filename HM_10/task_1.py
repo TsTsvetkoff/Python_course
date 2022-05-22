a9f108a653dbdd5f9060cfa06c98362a2d1c1a17
TsTsvetkoff <mukser@yahoo.com>
2022-05-22 19:38:25 +0300

    Home_work 10_task 1 _ polished up

--- HM_10/task_1.py
+++ HM_10/task_1.py
@@ -1,5 +1,7 @@
 import sqlite3 as sq
 import pandas as pd
+from termcolor import colored
+
 connection = sq.connect('information.db')
 curs = connection.cursor()
 curs.execute("create table if not exists inflationInfo" +
@@ -18,5 +20,20 @@ curs.execute('select * from inflationInfo')
 records = curs.fetchall()
 for row in records:
     print(row)
+
+curs.execute(
+    "SELECT * FROM inflationInfo WHERE inflation >=3 AND date_to >= '1999-01';"
+)
+inflation_high = curs.fetchall()
+print(f"Here is when (after 1999 year) the MONTHLY inflation % is greater or equal to 3 :\n{inflation_high}")
+print(colored("FYI 1999-2000 : Dot-com bubble burst\n"
+              "FYI 2006-2007 : Financial crisis triggered by Lehman Brothers collapse\n", 'green'))
+
+curs.execute(
+    "SELECT * FROM inflationInfo WHERE date_to >= '2022-01';"
+)
+apocalypse_now = curs.fetchall()
+
+print(colored(f"Where are we in 2022 in terms of crisis prediction (3% critical point)\n{apocalypse_now}", "red"))
 
 connection.close()
